33701d97c822a58bccafebc7966fc9ec22e330ef
jonfraser <fraser.jc@gmail.com>
2014-12-14 09:42:41 +1000

    initial commit of all files but config

---
+++ MNIT-Communication/Services/RegistrationService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MNIT_Communication.Services
+{
+    public class RegistrationService
+    {
+        private readonly string RegistrationQueue = "RegistrationQueue";
+        public void SendRegistrationRequest(string username, string email)
+        {
+            var connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+            var queueExists = namespaceManager.QueueExists(RegistrationQueue);
+            if(!queueExists)
+            {
+                namespaceManager.CreateQueue(RegistrationQueue);
+            }
+
+            var client = QueueClient.CreateFromConnectionString(connectionString, RegistrationQueue);
+            client.Send(new BrokeredMessage(username));
+
+        }
+    }
+}
---
+++ MNIT-Communication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MNIT_Communication.Startup))]
+namespace MNIT_Communication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
