3cc7d70d805d4d35dbb15e1b808698db6ced0bde
jonfraser <fraser.jc@gmail.com>
2014-12-21 23:20:40 +1000

    not much, just added some files and structure around the first rest endpoint to get the list of alerts

---
+++ MNIT-Communication/Areas/api/v1/Alerts/GetAlertables.cs
@@ -0,0 +1,25 @@
+﻿using MNIT_Communication.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MNIT_Communication.Areas.api.v1.Alerts
+{
+    public partial class Alertables : ApiController
+    {
+        public IEnumerable<Alertable> GetAlertables()
+        {
+            return new Alertable[] { 
+                new Alertable{ID=Guid.NewGuid(), Name="WardView" }
+                ,
+                new Alertable{ID = Guid.NewGuid(), Name="refer"}
+                ,
+                new Alertable{ID = Guid.NewGuid(), Name="FOCUS"}
+            };
+        }
+
+    }
+}
---
+++ MNIT-Communication/Controllers/AlertsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MNIT_Communication.Controllers
+{
+    public class AlertsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            //Do nothing, just return the view as we will ajax in the alerts via the rest api
+            return View();
+        }
+    }
+}
