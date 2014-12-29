c536f30fc86e6be585d1daf787ec2e5017152a39
jonfraser <fraser.jc@gmail.com>
2014-12-29 21:57:33 +1000

    switched the api endpoint to regular mvc, hooked up first displayvia angular

--- MNIT-Communication/App_Start/BundleConfig.cs
+++ MNIT-Communication/App_Start/BundleConfig.cs
@@ -8,8 +8,6 @@ namespace MNIT_Communication
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,7 +18,6 @@ namespace MNIT_Communication
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
