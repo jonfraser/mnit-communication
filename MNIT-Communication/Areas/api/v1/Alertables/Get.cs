f4fc10ba62155f6f257a55a3c09b13dd01c2144d
jonfraser <fraser.jc@gmail.com>
2014-12-29 21:58:11 +1000

    switched the api endpoint to regular mvc, hooked up first displayvia angular

---
+++ MNIT-Communication/Areas/api/v1/Alertables/Get.cs
@@ -0,0 +1,26 @@
+﻿using MNIT_Communication.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MNIT_Communication.Areas.api
+{
+    public partial class AlertablesController : System.Web.Mvc.Controller
+    {
+        public System.Web.Mvc.ActionResult Get()
+        {
+            return Json(new Alertable[] { 
+                new Alertable{ID=Guid.NewGuid(), Name="WardView" }
+                ,
+                new Alertable{ID = Guid.NewGuid(), Name="refer"}
+                ,
+                new Alertable{ID = Guid.NewGuid(), Name="FOCUS"}
+            }, System.Web.Mvc.JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
---
+++ MNIT-Communication/Areas/api/v1/Version/Get.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MNIT_Communication.Areas.api.v1.Version
+{
+    public partial class VersionController : System.Web.Mvc.Controller
+    {
+        public string Get()
+        {
+            return System.Reflection.Assembly.GetExecutingAssembly().FullName;
+        }
+
+    }
+}
