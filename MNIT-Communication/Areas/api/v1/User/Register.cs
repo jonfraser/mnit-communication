a331cbdf0bb264e9e758fee663993077687d10ec
jonfraser <fraser.jc@gmail.com>
2015-01-01 23:32:45 +1000

    switched the api endpoint to regular mvc, hooked up first displayvia angular

---
+++ MNIT-Communication/Areas/api/v1/User/Register.cs
@@ -0,0 +1,29 @@
+﻿using MNIT_Communication.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MNIT_Communication.Areas.api.v1
+{
+    public partial class UserController : ApiController
+    {
+        [HttpPost]
+        public async Task Register([FromBody]string emailAddress)
+        {
+            if (!emailAddress.ToLower().EndsWith("@health.qld.gov.au"))
+            {
+                throw new NotSupportedException("The email address used must be your health address.");
+            }
+            var svc = new RegistrationService();
+            await svc.SendRegistrationRequest(emailAddress);
+
+            //TODO: Somehow (maybe in the above service) we need to partially register this user before sending them to the next step
+
+        }
+
+    }
+}
