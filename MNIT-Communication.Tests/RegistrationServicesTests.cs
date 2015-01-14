87d793b0ab82a442de4767fa8540866e9dbe1a44
jonfraser <fraser.jc@gmail.com>
2015-01-14 22:39:25 +1000

    include test project artefacts

---
+++ MNIT-Communication.Tests/RegistrationServicesTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MNIT_Communication.Services;
+
+namespace MNIT_Communication.Tests
+{
+	[TestClass]
+	public class RegistrationServicesTests
+	{
+		[TestMethod]
+		public void ProcessServiceBusRegistrationMessageWithValidParamsShouldStoreTokenAndSendEmail()
+		{
+			IRegistrationService reg = new RegistrationService();
+			reg.ProcessServiceBusRegistrationMessage("http://localhost:13821", new Domain.NewUserRegistrationBrokeredMessage
+				{
+					CorrelationId = Guid.NewGuid(),
+					EmailAddress = "jon.fraser@health.qld.gov.au"
+				});
+		}
+	}
+}
