72b2ffaa39a2d91c48f5255f35ede18d247aaaa3
jonfraser <fraser.jc@gmail.com>
2015-01-27 22:52:15 +1000

    add in some event logging now that I have proven that the call is making it to the api

--- MNIT-Communication/Areas/api/v1/User/ProcessRegistration.cs
+++ MNIT-Communication/Areas/api/v1/User/ProcessRegistration.cs
@@ -2,6 +2,8 @@
 using MNIT_Communication.Services;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Diagnostics;
+using System;
 
 namespace MNIT_Communication.Areas.api.v1
 {
@@ -10,7 +12,18 @@ namespace MNIT_Communication.Areas.api.v1
 		[HttpPost]
 		public async Task ProcessRegistration(NewUserRegistrationBrokeredMessage newUserRegistrationBrokeredMessage)
 		{
-			await registrationService.ProcessRegistrationRequest(newUserRegistrationBrokeredMessage.CorrelationId, newUserRegistrationBrokeredMessage.EmailAddress);
+			EventLog.WriteEntry("Application", "CorrelationID: " + newUserRegistrationBrokeredMessage.CorrelationId.ToString(), EventLogEntryType.Information);
+			EventLog.WriteEntry("Application", "Email: " + newUserRegistrationBrokeredMessage.EmailAddress, EventLogEntryType.Information);
+
+			try
+			{
+				await registrationService.ProcessRegistrationRequest(newUserRegistrationBrokeredMessage.CorrelationId, newUserRegistrationBrokeredMessage.EmailAddress);
+			}
+			catch (Exception ex)
+			{
+				EventLog.WriteEntry("Application", ex.ToString(), EventLogEntryType.Error);
+				throw;
+			}
 		}
 
 
