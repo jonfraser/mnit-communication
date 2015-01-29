86bf774c68d6dac983b54298e51dba0e359886e9
jonfraser <fraser.jc@gmail.com>
2015-01-29 20:18:20 +1000

    add in message queue processing via web job

---
+++ MNIT-Communication.Jobs.NewUserRegistration/Functions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure;
+using MNIT_Communication.Domain;
+using MNIT_Communication.Services;
+
+namespace MNIT_Communication.Jobs.NewUserRegistration
+{
+	public class Functions
+	{
+
+		// This function will get triggered/executed when a new message is written 
+		// on an Azure Queue called queue.
+		public static void ProcessQueueMessage([QueueTrigger("RegistrationQueue")] NewUserRegistrationBrokeredMessage message, TextWriter log)
+		{
+			log.WriteLine("Received message from queue");
+			new RegistrationService().ProcessServiceBusRegistrationMessage(
+							CloudConfigurationManager.GetSetting("BaseWebUrl"),
+							message).RunSynchronously();
+		}
+	}
+}
--- MNIT-Communication/Areas/api/v1/User/ProcessRegistration.cs
+++ MNIT-Communication/Areas/api/v1/User/ProcessRegistration.cs
@@ -12,6 +12,7 @@ namespace MNIT_Communication.Areas.api.v1
 		[HttpPost]
 		public async Task ProcessRegistration(NewUserRegistrationBrokeredMessage newUserRegistrationBrokeredMessage)
 		{
+			//todo: both these are null but the object itself doesn't seem to be
 			EventLog.WriteEntry("Application", "CorrelationID: " + newUserRegistrationBrokeredMessage.CorrelationId.ToString(), EventLogEntryType.Information);
 			EventLog.WriteEntry("Application", "Email: " + newUserRegistrationBrokeredMessage.EmailAddress, EventLogEntryType.Information);
 
