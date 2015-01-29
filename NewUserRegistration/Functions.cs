552aac2d002ae0ae1df1a17083abdf9a749bd6b9
jonfraser <fraser.jc@gmail.com>
2015-01-29 22:08:50 +1000

    add in message queue processing via web job the correct way

--- NewUserRegistration/Functions.cs
+++ NewUserRegistration/Functions.cs
@@ -5,16 +5,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure;
+using MNIT_Communication.Domain;
+using MNIT_Communication.Services;
 
 namespace NewUserRegistration
 {
 	public class Functions
 	{
-		// This function will get triggered/executed when a new message is written 
-		// on an Azure Queue called queue.
-		public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
+		public static void ProcessQueueMessage([QueueTrigger("RegistrationQueue")] NewUserRegistrationBrokeredMessage message, TextWriter log)
 		{
-			log.WriteLine(message);
+			log.WriteLine("Received message from queue");
+			new RegistrationService().ProcessServiceBusRegistrationMessage(
+							CloudConfigurationManager.GetSetting("BaseWebUrl"),
+							message).RunSynchronously();
 		}
 	}
 }
