f65a0747c9b80aed9ed1d78d5e4912a416bedda3
jonfraser <fraser.jc@gmail.com>
2015-01-29 22:24:28 +1000

    so apparently my webjob exe class needs to be public?

--- NewUserRegistration/Program.cs
+++ NewUserRegistration/Program.cs
@@ -8,11 +8,11 @@ using Microsoft.Azure.WebJobs;
 namespace NewUserRegistration
 {
 	// To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
-	class Program
+	public class Program
 	{
 		// Please set the following connection strings in app.config for this WebJob to run:
 		// AzureWebJobsDashboard and AzureWebJobsStorage
-		static void Main()
+		public static void Main()
 		{
 			var host = new JobHost();
 			// The following code ensures that the WebJob will be running continuously
