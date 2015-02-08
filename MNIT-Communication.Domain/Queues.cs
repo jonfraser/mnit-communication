def1e8f52a89ee0b43edd359b8307fcf2840b131
jonfraser <fraser.jc@gmail.com>
2015-02-08 16:29:17 +1000

    centralise the queue names, easier to track this way

---
+++ MNIT-Communication.Domain/Queues.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNIT_Communication.Domain
+{
+	public static class Queues
+	{
+		public const string Registration = "RegistrationQueue";
+		public const string Alerts = "AlertsQueue";
+		public const string MobileNumberVerify = "MobileNumberVerify";
+
+	}
+}
