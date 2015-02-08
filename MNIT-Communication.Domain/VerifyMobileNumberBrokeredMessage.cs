b7e01b1c88369cc35d28a1d394355969a511600c
jonfraser <fraser.jc@gmail.com>
2015-02-08 16:41:08 +1000

    verify mobile number job added but does not do anything at the moment

---
+++ MNIT-Communication.Domain/VerifyMobileNumberBrokeredMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNIT_Communication.Domain
+{
+	public class VerifyMobileNumberBrokeredMessage
+	{
+		public Guid CorrelationId { get; set; }
+		public Guid NewUserRegistrationId { get; set; }
+		public string MobileNumber { get; set; }
+	}
+}
---
+++ VerifyMobileNumberJob/Functions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using MNIT_Communication.Domain;
+using MNIT_Communication.Services;
+
+namespace VerifyMobileNumberJob
+{
+	public class Functions
+	{
+		public async static Task ProcessQueueMessage([ServiceBusTrigger(Queues.MobileNumberVerify)] VerifyMobileNumberBrokeredMessage message, TextWriter log)
+		{
+			log.WriteLine("Received message " + message.CorrelationId.ToString() + " from queue for " + message.MobileNumber);
+			var svc = new RegistrationService();
+
+			try
+			{
+				//await svc.ProcessServiceBusRegistrationMessage(message);
+			}
+			catch (Exception ex)
+			{
+				log.WriteLine(string.Format("Error in ProcessMessageQueue: {0}", ex.ToString()));
+				throw;
+			}
+		}
+	}
+}
