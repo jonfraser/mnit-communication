a3c236aaf9089040b0a89620e929a43b2c3cb9c3
sjperske <sjperske@gmail.com>
2015-07-24 16:44:26 +1000

    Adding this new Test

---
+++ MNIT-Communication.Tests/Services/SendTelstraSmsServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MNIT_Communication.Services;
+using Xunit;
+
+namespace MNIT_Communication.Tests.Services
+{
+    public class SendTelstraSmsServiceTests
+	{
+        //[Fact, TestCategory("Integration")]
+        //public async Task CanSendSmsThroughTelstra()
+        //{
+        //    var sms = new SendTelstraSmsService();
+        //    await sms.SendSimple("0466646967", "Love you! This is what I'm playing with :p");
+        //}
+
+        [Fact, TestCategory("Integration")]
+        public async Task CanSendSmsThroughTelstra()
+        {
+            var sms = new SendTelstraSmsService();
+            await sms.SendSimple("0400099743", "Test Text Messaiag via Telstra API :p");
+        }
+	}
+}
