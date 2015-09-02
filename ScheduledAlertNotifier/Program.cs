bfaaf4e5c76b3e62aa8caa829c2d2c69e82bd5fa
Steven Perske <steven@notion.net.au>
2015-09-02 15:59:17 +1000

    Removing WebJob SDK and back to bare metal for this baby

--- ScheduledAlertNotifier/Program.cs
+++ ScheduledAlertNotifier/Program.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using Microsoft.Azure.WebJobs;
 using Microsoft.WindowsAzure;
 using MNIT.ErrorLogging;
+using MNIT_Communication.Domain;
 using MNIT_Communication.Services;
 
 namespace ScheduledAlertNotifier
 {
     public class Program
     {
+        private static IAuditService auditService;
+        private static IErrorLogger errorLogger;
+        private static IAlertsService alertService;
+        
         public static void Main()
         {
 
@@ -24,9 +30,54 @@ namespace ScheduledAlertNotifier
 
             ServiceLocator.RegisterType<AlertsService>().As<IAlertsService>();
 
-            var host = new JobHost();
-            // The following code ensures that the WebJob will be running continuously
-            host.RunAndBlock();
+            CheckForScheduledAlerts();
+        }
+
+        public static void CheckForScheduledAlerts()
+        {
+            Console.WriteLine("CheckForScheduledAlerts called");
+
+            auditService = ServiceLocator.Resolve<IAuditService>();
+            errorLogger = ServiceLocator.Resolve<IErrorLogger<Guid>>();
+            alertService = ServiceLocator.Resolve<IAlertsService>();
+
+            var intervalSeconds = 60; //Default
+            int.TryParse(CloudConfigurationManager.GetSetting("ScheduledAlertNotifier.IntervalSeconds"), out intervalSeconds);
+            var milliseonds = TimeSpan.FromSeconds(intervalSeconds).TotalMilliseconds;
+
+            using (var timer = new Timer(milliseonds))
+            {
+                timer.Elapsed += DoNotifications;
+
+                timer.Enabled = true;
+                timer.Start();
+
+                Console.WriteLine("Timer started");
+            }
+        }
+
+        private static void DoNotifications(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("DoNotifications called");
+
+            Task.Run(async () =>
+            {
+                try
+                {
+                    auditService.LogAuditEvent(new AuditEvent
+                    {
+                        AuditType = AuditType.ScheduledAlertsNotified,
+                        Details = "CheckForScheduledAlerts was called"
+                    });
+
+                    await alertService.NotifyScheduledAlerts();
+                    Console.WriteLine("alertService.NotifyScheduledAlerts called");
+                }
+                catch (Exception ex)
+                {
+                    errorLogger.LogError(ex);
+                }
+            });
         }
     }
 }
